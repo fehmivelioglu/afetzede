b822ab515c5e7e26a36aaca1cf2035aebf88eed5
tahsin-emre <tahsin_emre37@hotmail.com>
2021-02-06 01:56:57 +0300

    01:56

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'views/screens/home/homeView.dart';
 
 void main() => runApp(MyApp());
 
@@ -6,17 +7,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      title: 'AFETZEDE',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeView(),
+      },
     );
   }
 }
