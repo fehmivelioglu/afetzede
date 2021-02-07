1d32c215761c387a3bf681b6e9766d6f8cdd8443
tahsin-emre <tahsin_emre37@hotmail.com>
2021-02-07 04:18:02 +0300

    test2

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'views/screens/home/homeView.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
+void main() {
   runApp(MyApp());
 }
 
